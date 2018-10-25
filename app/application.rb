5e29ad16bc3c60a35bac0bff694174f725727486
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-25 19:36:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    
+    now = Time.now
+    if now.hour >= 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
 
     resp.finish
  end
